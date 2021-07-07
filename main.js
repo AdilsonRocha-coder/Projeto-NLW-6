18633963e7abed5a452fc9a9a79c7e1576d935dc
AdilsonRocha-coder <piuwave@hotmail.com>
2021-07-06 22:59:07 -0300

    terminado o javascript do menu - toggle

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 // DOM Document Object Model
+
+/* ABRE E FECHA O MENU QUANDO CLICAR NO ICONE "HAMBURGER" E "X" */
 const nav = document.querySelector('#header nav')  //aqui dizemos ao 'document' para procurar todo o seletor que tenha #header e o nav, e colocar eles dentro da constante 'nav'.
 const toggle = document.querySelectorAll('nav .toggle')  //aqui dizemos ao 'toggle' para procurar todo o seletor que tenha o nav e a classe togle, e colocar eles dentro da constante 'toggle'.
 
@@ -11,4 +13,14 @@ for (const element of toggle) {
     })
 }
 //a função acima dia para o 'nav' colocar a classe 'show' (caso ela não tenha) ou retire a classe 'show' caso ela tenha. Tod vez que clicarmos no ícone do menu.
+
+// a const 'element' é a 'div' que tem a class 'toggle icon-menu' e 'toggle icon-close'
+
+/* QUANDO CLICAR EM UM ITEM DO MENU, ESCONDER O MENU */
+const links = document.querySelectorAll('nav ul li a')
+for (const link of links) {
+    link.addEventListener('click', function () {
+        nav.classList.remove('show')
+    })
+}
 
