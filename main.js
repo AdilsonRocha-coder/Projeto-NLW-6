7e0c769901122551d4ad9c9277096e37b59ffeef
AdilsonRocha-coder <piuwave@hotmail.com>
2021-07-14 19:35:25 -0300

    Finished the footer and some changes on the scroll function

--- main.js
+++ main.js
@@ -24,8 +24,72 @@ for (const link of links) {
 }
 
 /* mudar o header da página quando executarmos o scroll */
+function changeHeaderWhenScroll() {
+  const header = document.querySelector("#header");
+  const navHeight = header.offsetHeight; //aqui pegamos o deslocamento da altura (offset) do header
+
+  if (this.window.scrolly >= navHeight) {
+    //aqui se o scroll é maior ou igual à altura do header fazemos algo
+    header.classList.add("scroll");
+  } else {
+    // menor que a altura do header fazemos outra coisa
+    header.classList.remove("scroll");
+  }
+}
+
+/* TESTIMONIALS SLIDER (CAROUSEL) SWIPER */
+const swiper = new Swiper(".swiper-container", {
+  slidesPerView: 1,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
+});
+
+/* SCROLLREVEAL - ELE MOSTRA OS ELEMENTOS QUANDO DAR O SCROLL NA PÁGINA */
+const scrollReveal = ScrollReveal({
+  origin: "top",
+  distance: "30px",
+  duration: 700,
+  reset: true,
+});
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+    #about .image, #about .text,
+    #services header, #services.card,
+    #testimonials header, #testimonials .testimonials,
+    #contact .text, #contact .links,
+    footer .brand, footer .social
+    `,
+  { interval: 100 }
+);
+
+/* BOTÃO VOLTAR PARA O TOPO */
+function backToTop() {
+  const backToTopButton = document.querySelector(".back-to-top");
+
+  if (this.window.scrollY >= 600) {
+    backToTopButton.classList.add("show");
+  } else {
+    backToTopButton.classList.remove("show");
+  }
+}
+
+/* WHEN SCROLL */
+window.addEventListener("scroll", function () {
+  changeHeaderWhenScroll();
+  backToTop();
+});
+
+/* inicialmente o Mayk tinha feito essa lógica para fazermos o scroll. Depois ele mostrou 
+um jeito mais elaborado/eficiente que é o que estamos usando acima no "mudar o header da página quando executarmos o scroll" 
+
+esse código estava na linha 26 à 38
+
 const header = document.querySelector("#header");
-const navHeight = header.offsetHeight; //aqui pegamos o deslocamento da altura (offset) do header
+  const navHeight = header.offsetHeight; //aqui pegamos o deslocamento da altura (offset) do header
 
 window.addEventListener("scroll", function () {
   if (this.window.scrolly >= navHeight) {
@@ -37,30 +101,4 @@ window.addEventListener("scroll", function () {
   }
 });
 
-/* TESTIMONIALS SLIDER (CAROUSEL) SWIPER */
-const swiper = new Swiper(".swiper-container", {
-  slidesPerView: 1,
-  pagination: {
-    el: ".swiper-pagination"
-  },
-  mousewheel: true,
-  keyboard: true,
-});
-
-/* SCROLLREVEAL - ELE MOSTRA OS ELEMENTOS QUANDO DAR O SCROLL NA PÁGINA */
-const scrollReveal = ScrollReveal({
-    origin: 'top',
-    distance: '30px',
-    duration: 700,
-    reset: true
-})
-
-scrollReveal.reveal(
-    `#home .image, #home .text,
-    #about .image, #about .text,
-    #services header, #services.card,
-    #testimonials header, #testimonials .testimonials,
-    #contact .text, #contact .links
-    `,
-{  interval: 100}
-)
+*/
